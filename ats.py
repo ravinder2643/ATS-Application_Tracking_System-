70be13e9a0786a996c2283a7a3136307ad5d6e54
ravinder2643 <138236210+ravinder2643@users.noreply.github.com>
2024-05-21 00:22:41 +0530

    initial commit

---
+++ ats.py
@@ -0,0 +1,46 @@
+from dotenv import load_dotenv # type: ignore
+
+load_dotenv()
+import base64
+import streamlit as st # type: ignore
+import os
+import io
+from PIL import Image  # type: ignore
+import pdf2image # type: ignore
+import google.generativeai as genai # type: ignore
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+def get_gemini_response(input,pdf_cotent,prompt):
+    model=genai.GenerativeModel('gemini-pro-vision')
+    response=model.generate_content([input,pdf_cotent[0],prompt])
+    return response.text
+
+def input_pdf_setup(uploaded_file):
+    if uploaded_file is not None:
+        ## Convert the PDF to image
+        images=pdf2image.convert_from_bytes(uploaded_file.read())
+
+        first_page=images[0]
+
+        # Convert to bytes
+        img_byte_arr = io.BytesIO()
+        first_page.save(img_byte_arr, format='JPEG')
+        img_byte_arr = img_byte_arr.getvalue()
+
+        pdf_parts = [
+            {
+                "mime_type": "image/jpeg",
+                "data": base64.b64encode(img_byte_arr).decode()  # encode to base64
+            }
+        ]
+        return pdf_parts
+    else:
+        raise FileNotFoundError("No file uploaded")
+
+
+   
+
+
+
+
